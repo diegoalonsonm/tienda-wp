2353cd74e13e9b3b899c91bcd122cc3561eb08b7
Diego <diegoalonsonm@gmail.com>
2024-08-06 19:54:37 -0600

    sem12

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,10 +1,23 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
+        var reader = new FileReader()
         reader.onload = (e) => {
             $('#blah').attr('src', e.target.result).height(200)
         }
         
         reader.readAsDataURL(input.files[0])
     }
+}
+
+//La siguiente función, agrega en el carrito de compras un producto
+//En la variable de sesión items, hace un llamado Ajax
+function addCart(formulario) {
+    var idProducto = formulario.elements[0].value
+    var existencias = formulario.elements[1].value
+    if (existencias > 0) {
+        var url = "/carrito/agregar/" + idProducto
+        $("#resultsBlock").load(url)
+    }
+    
+    console.log('hola se estripo')
 }
